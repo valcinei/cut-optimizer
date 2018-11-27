a35b5333218efad121a0a49d6c7758931c796ad6
Valcinei Silva <valcinei.silva@fpf.br>
2018-11-27 17:10:19 -0400

    config tests

---
+++ src/spec/test.spec.ts
@@ -0,0 +1,21 @@
+
+import { CutOptimizer } from '../Optimizer/CutOptimizer';
+import { Shape } from '../Optimizer/ICutOptimizer';
+let cut_opt = new CutOptimizer(100,100);
+let shapes = [
+    new Shape(100,100),
+    new Shape(100,100),
+    new Shape(100,100),
+    new Shape(100,100),
+    new Shape(100,100),
+    new Shape(100,100),
+    new Shape(100,100),
+    new Shape(100,100)
+] 
+    
+
+describe("Create Cut Opitimizer", () => {
+    it("Has optimized shapes", () => {
+        expect(cut_opt.optimize(shapes).items.length).toBeGreaterThan(0);
+    }); 
+} );
