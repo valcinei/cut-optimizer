961426583a684d48ffc4d2df611c24c1f27bb111
Valcinei Silva <valcinei.silva@fpf.br>
2018-11-28 12:12:33 -0400

    add tests

--- src/specs/CutOptimizer.spec.ts
+++ src/specs/CutOptimizer.spec.ts
@@ -11,10 +11,26 @@ let shapes = [
     new Shape(100,100),
     new Shape(100,100),
     new Shape(100,100)
-];     
-
-describe("Create Cut Opitimizer", () => {
+];
+describe("CutOpitimizer", () => {
+    
     it("Has optimized shapes", () => {
         expect(cut_opt.optimize(shapes).items.length).toBeGreaterThan(0);
+    });
+
+    it("Has X coordinate", () => {
+        cut_opt.optimize(shapes).items.forEach((item)=>expect(item.x).not.toBeNull());
     }); 
+
+    it("Has Y coordinate", () => {
+        cut_opt.optimize(shapes).items.forEach((item)=>expect(item.y).not.toBeNull());
+    });
+
+    it("Has X coordinate number", () => {
+        cut_opt.optimize(shapes).items.forEach((item)=>expect(item.x).toEqual(jasmine.any(Number)));
+    });
+    it("Has Y coordinate number", () => {
+        cut_opt.optimize(shapes).items.forEach((item)=>expect(item.y).toEqual(jasmine.any(Number)));
+    });
+    
 } );
